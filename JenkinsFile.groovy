f748434326fd593a469f0c57c1e1a7f6b1687377
Nishad Mehendale <26198718+nishadmehendale@users.noreply.github.com>
2018-11-11 14:21:52 +0530

    Uploaded Groovy

---
+++ JenkinsFile.groovy
@@ -0,0 +1,18 @@
+node {
+   def mvnHome
+   stage('Preparation') { // for display purposes
+      // Get some code from a GitHub repository
+      git 'https://github.com/nishadmehendale/Spring-Boot-Freestyle'
+      // Get the Maven tool.
+      // ** NOTE: This 'M3' Maven tool must be configured
+      // **       in the global configuration.           
+      mvnHome = tool 'M3'
+   }
+   stage('Build') {
+      // Run the maven build
+         sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
+   }
+   stage('Results') {
+      archive 'target/*.jar'
+   }
+}
